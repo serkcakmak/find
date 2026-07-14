cf6659b42ee1502835a9f7c637bea74ec41ee83e
Serkan Cakmak <serkan@serkancakmak.com.tr>
2026-07-14 20:37:59 +0300

    fix: Type error on getStockCandles

--- src/lib/yahoo-finance.ts
+++ src/lib/yahoo-finance.ts
@@ -1,6 +1,6 @@
 import yahooFinance from 'yahoo-finance2';
 
-export async function getStockCandles(symbol: string, from: Date, to: Date) {
+export async function getStockCandles(symbol: string, from: Date, to: Date): Promise<any[] | null> {
   try {
     const queryOptions = { 
       period1: from, 
